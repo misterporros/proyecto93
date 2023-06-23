0739b9dc9d893a072f273512b0ab271181dc430f
misterporros <136129718+misterporros@users.noreply.github.com>
2023-06-23 15:48:32 -0600

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,13 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyAmzTWas208REGCUWqr3L4DS9NuwZLCmEg",
+    authDomain: "vamos-a-chatear-parte222222x.firebaseapp.com",
+    databaseURL: "https://vamos-a-chatear-parte222222x-default-rtdb.firebaseio.com",
+    projectId: "vamos-a-chatear-parte222222x",
+    storageBucket: "vamos-a-chatear-parte222222x.appspot.com",
+    messagingSenderId: "935316542937",
+    appId: "1:935316542937:web:194a483264cde138cd75c8"
+  };
+  
+  // Initialize Firebase
+firebase.initializeApp(firebaseConfig);
